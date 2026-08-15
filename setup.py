c31fa84c5503d8112f03fdeae6be1d661aa32f00
Arsha <arsha.dadvar@gmail.com>
2026-08-15 15:38:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fx_library_pro',
-    version='1.0.2',
+    version='1.0.5',
     description='this is a fx_library_pro',
     author='Arsha'
 )
